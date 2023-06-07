2c1a319f6d751ed2afd59687b22c23d2d3744993
Mohan Swaroop <poolamohanswarooplnx@gmail.com>
2023-06-07 10:23:27 +0530

    basic linux commands V1.0

---
+++ basic-linux-commands.sh
@@ -0,0 +1,20 @@
+#LInux Basic Commands:
+hostname
+hostnamectl
+hostnamectl set-hostname testserver.com
+exec bash
+cat /etc/os-release
+cat /etc/redhat-release
+who
+users
+pwd
+who am i
+ls
+ls -l
+ll 
+ls -a
+ls -la
+ll -a
+date
+cal
+
